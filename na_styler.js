2c691048b3a89a8d426ec06e74843e5c2aeed944
Anthony Arias <aarias011@west-mec.org>
2019-04-02 18:28:02 -0700

    Today I
    Put a function that allows one of five fancy style sheets to be randomly used when the page is opened, allowed users to be able to choose between the five fancy style sheet themes she has created by clicking thumbnail images from a figure box, populated the figure box w/ preview images of the 5 fancy style sheets, and made the browser load a different style sheet when the user clicks one of the thumbnail images.

---
+++ na_styler.js
@@ -0,0 +1,70 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5, CSS3 and JavaScript 6th Edition
+   Tutorial 12
+   Case Problem 1
+
+   Author: Anthony Arias
+   Date:   4/2/19
+
+   Filename: na_styler.js
+
+   Functions
+   =========
+   
+   setStyles()
+      Sets up the style sheets and the style sheet switcher.
+      
+   randInt(size)
+      Returns a random integer from 0 up to size-1.
+
+*/
+
+window.addEventListener("load", setStyles);
+//this allows one of five fancy style sheets to be randomly used when the page is opened
+function setStyles() {
+      var styleNum = randInt(5);
+      var fancy = document.createElement("link");
+      fancy.setAttribute("rel", "stylesheet");
+      fancy.setAttribute("id", "fancySheet");
+      fancy.setAttribute("href", "na_style_" + styleNum + ".css");
+      document.head.appendChild(fancy);
+
+      //this allows users to be able to choose between the five fancy style sheet themes she has created by clicking thumbnail images from a figure box
+      var figBox = document.createElement("figure");
+      figBox.setAttribute("id", "styleThumbs");
+      document.getElementById("box").appendChild(figBox);
+
+      //this populates the figure box w/ preview images of the 5 fancy style sheets.
+      for (var i = 0; i <= 4; i++) {
+            var sheetIMG = document.createElement("img");
+            sheetIMG.setAttribute("src", "na_small_" + i + ".png");
+            sheetIMG.setAttribute("alt", "na_style_" + i + ".css");
+
+            // this makes the browser load a different style sheet when the user clicks one of the thumbnail images
+            sheetIMG.onclick = function (e) {
+                  fancy.setAttribute("href", e.target.alt);
+                  a
+            }
+            figBox.appendChild(sheetIMG);
+      }
+}
+
+var thumbStyles = document.createElement("style");
+document.head.appendChild(thumbStyles);
+
+document.styleSheets[document.styleSheets.length - 1].insertRule(
+      "figure#styleThumbs { \
+      position: absolute; \
+      left: 0px; \
+      bottom: 0px; \
+      }", 0);
+document.styleSheets[document.styleSheets.length - 1].insertRule();
+
+
+
+
+function randInt(size) {
+      return Math.floor(size * Math.random());
+}
